55130dc317e5007f3fe85de780eac6cb26b27eec
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2021-01-01 14:41:35 -0300

    Em processo

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -1,13 +1,13 @@
 'use strict'
-
+const User = use("App/Models/User")
 class AuthController {
 
     async authenticater({ request, auth }) {
         const { email, password } = request.all()
         const token = await auth.attempt(email, password)
-        return token
+        const user = await User.findBy({ email })
+        return { token, user }
     }
-
 }
 
 module.exports = AuthController
