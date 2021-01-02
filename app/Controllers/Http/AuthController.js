dde7d83d9ee0189d21714370bd4bf956a8a59a1a
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2021-01-02 18:33:06 -0300

    Em processo

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -2,11 +2,27 @@
 const User = use("App/Models/User")
 class AuthController {
 
-    async authenticater({ request, auth }) {
+    async authenticater({ request, response, auth }) {
         const { email, password } = request.all()
-        const token = await auth.attempt(email, password)
-        const user = await User.findBy({ email })
-        return { token, user }
+        if (!(email && password))
+            response.status(400).send([{
+                message: 'Informe o e-email e a senha para efetuar login.'
+            }])
+
+        else {
+            const user = await User.findBy({ email })
+            if (!user)
+                return response.status(400).send([{
+                    message: 'E-mail não registrado.'
+                }])
+            if (!user.active)
+                return response.status(400).send([{
+                    message: 'Sua conta está desativada.'
+                }])
+            const token = await auth.attempt(email, password)
+            return { token, user }
+
+        }
     }
 }
 
