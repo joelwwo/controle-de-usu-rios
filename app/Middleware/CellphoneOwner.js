1fe25515fee418420092bc374e46912e7f094689
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2021-01-02 16:26:33 -0300

    Em processo

--- app/Middleware/CellphoneOwner.js
+++ app/Middleware/CellphoneOwner.js
@@ -3,15 +3,28 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Cellphone = use("App/Models/Cellphone")
 class CellphoneOwner {
   /**
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Function} next
    */
-  async handle ({ request }, next) {
-    // call next to advance the request
+  async handle({ request, params, response }, next) {
+
+    const data = request.only(["number"])
+    const { number } = data
+    const targetCellphoneId = params.id
+    const cellphone = await Cellphone.findBy({ number })
+
+    if (!cellphone) await next()
+    if (cellphone.id != targetCellphoneId)
+      return response.status(400).send([{
+        message: "Esse número já está em uso em outra conta."
+      }])
+
     await next()
+
   }
 }
 
