92110930da95752d563651255036624db8373a18
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2021-04-03 14:24:43 -0300

    Retirada de validação

--- start/routes.js
+++ start/routes.js
@@ -1,6 +1,6 @@
-'use strict'
+"use strict";
 
-const { RouteResource } = require('@adonisjs/framework/src/Route/Manager')
+const { RouteResource } = require("@adonisjs/framework/src/Route/Manager");
 
 /*
 |--------------------------------------------------------------------------
@@ -16,82 +16,97 @@ const { RouteResource } = require('@adonisjs/framework/src/Route/Manager')
 */
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
+const Route = use("Route");
 
-Route.get('/', ({ request }) => {
-  if (Object.entries(request.all()).length) return request.all()
-  return { message: 'Beleza, Matos', by: 'Joel' }
-})
+Route.get("/", ({ request }) => {
+  if (Object.entries(request.all()).length) return request.all();
+  return { message: "Beleza, Matos", by: "Joel" };
+});
 
-Route.post('/login', 'AuthController.authenticater')
+Route.post("/login", "AuthController.authenticater");
 
-Route.resource('users', 'UserController')
-  .middleware(new Map([
-    [['index'], ['auth', 'typeUser:master,query,edit']],
-    [['store'], ['auth', 'typeUser:master,edit', 'cpfOrEmailOwner']],
-    [['show'], ['auth', 'typeUser:master,query,edit,self']],
-    [['update'], ['auth', 'typeUser:master,self', 'cpfOrEmailOwner']],
-    [['destroy'], ['auth', 'typeUser:master']],
-  ]))
-  .validator(new Map([
-    [['users.store'], ['StoreUser']],
-    [['users.update'], ['UpdateUser']]
-  ]))
-  .apiOnly()
+Route.resource("users", "UserController")
+  .middleware(
+    new Map([
+      [["index"], ["auth", "typeUser:master,query,edit"]],
+      [["store"], ["auth", "typeUser:master,edit"]],
+      [["show"], ["auth", "typeUser:master,query,edit,self"]],
+      [["update"], ["auth", "typeUser:master,self", "cpfOrEmailOwner"]],
+      [["destroy"], ["auth", "typeUser:master"]],
+    ])
+  )
+  .validator(
+    new Map([
+      [["users.store"], ["StoreUser"]],
+      [["users.update"], ["UpdateUser"]],
+    ])
+  )
+  .apiOnly();
 
+Route.resource("address", "AddressController")
+  .middleware(
+    new Map([
+      [["index"], ["auth", "typeUser:master,query,edit"]],
+      [["store"], ["auth", "typeUser:master,edit"]],
+      [["show"], ["auth", "typeUser:master,query,self,edit"]],
+      [["update"], ["auth", "typeUser:master,self,edit"]],
+      [["destroy"], ["auth", "typeUser:master,self"]],
+    ])
+  )
+  .validator(
+    new Map([
+      [["address.store"], ["StoreAddress"]],
+      [["address.update"], ["UpdateAddress"]],
+    ])
+  )
+  .apiOnly();
 
-Route.resource('address', 'AddressController')
-  .middleware(new Map([
-    [['index'], ['auth', 'typeUser:master,query,edit']],
-    [['store'], ['auth', 'typeUser:master,edit']],
-    [['show'], ['auth', 'typeUser:master,query,self,edit']],
-    [['update'], ['auth', 'typeUser:master,self,edit']],
-    [['destroy'], ['auth', 'typeUser:master,self']],
-  ]))
-  .validator(new Map([
-    [['address.store'], ['StoreAddress']],
-    [['address.update'], ['UpdateAddress']]
-  ]))
-  .apiOnly()
+Route.resource("cellphones", "CellphoneController")
+  .middleware(
+    new Map([
+      [["index"], ["auth", "typeUser:master,query,edit"]],
+      [["store"], ["auth", "typeUser:master,edit"]],
+      [["show"], ["auth", "typeUser:master,query,edit,self"]],
+      [["update"], ["auth", "typeUser:master,edit,self", "cellphoneOwner"]],
+      [["destroy"], ["auth", "typeUser:master,self"]],
+    ])
+  )
+  .validator(
+    new Map([
+      [["cellphones.store"], ["StoreCellphone"]],
+      [["cellphones.update"], ["UpdateCellphone"]],
+    ])
+  )
+  .apiOnly();
 
-Route.resource('cellphones', 'CellphoneController').middleware(new Map([
-  [['index'], ['auth', 'typeUser:master,query,edit']],
-  [['store'], ['auth', 'typeUser:master,edit']],
-  [['show'], ['auth', 'typeUser:master,query,edit,self']],
-  [['update'], ['auth', 'typeUser:master,edit,self', 'cellphoneOwner']],
-  [['destroy'], ['auth', 'typeUser:master,self']],
-]))
-  .validator(new Map([
-    [['cellphones.store'], ['StoreCellphone']],
-    [['cellphones.update'], ['UpdateCellphone']]
-  ]))
-  .apiOnly()
-
-Route.resource('shoppinglists', 'ShoppinglistController')
-  .middleware(new Map([
-    [['index'], ['auth', 'typeUser:master,query,edit']],
-    [['store'], ['auth', 'typeUser:master,edit']],
-    [['show'], ['auth', 'typeUser:master,query,edit,self']],
-    [['update'], ['auth', 'typeUser:master']],
-    [['destroy'], ['auth', 'typeUser:master']],
-  ]))
-  .validator(new Map([
-    [['shoppinglists.store'], ['StoreShoppinglist']],
-    [['shoppinglists.update'], ['UpdateShoppinglist']]
-  ]))
-  .apiOnly()
+Route.resource("shoppinglists", "ShoppinglistController")
+  .middleware(
+    new Map([
+      [["index"], ["auth", "typeUser:master,query,edit"]],
+      [["store"], ["auth", "typeUser:master,edit"]],
+      [["show"], ["auth", "typeUser:master,query,edit,self"]],
+      [["update"], ["auth", "typeUser:master"]],
+      [["destroy"], ["auth", "typeUser:master"]],
+    ])
+  )
+  .validator(
+    new Map([
+      [["shoppinglists.store"], ["StoreShoppinglist"]],
+      [["shoppinglists.update"], ["UpdateShoppinglist"]],
+    ])
+  )
+  .apiOnly();
 
 Route.group(() => {
-
-  Route.get('getAllPurchases/:id', 'UserController.getAllPurchases')
-  Route.get('getAllPaidPurchases/:id', 'UserController.getAllPaidPurchases')
-  Route.get('getAllUnPaidPurchases/:id', 'UserController.getAllUnPaidPurchases')
-  Route.get('getAllAdmin', 'UserController.getAllAdmin')
-  Route.get('findBy', 'UserController.findBy')
-  Route.get('findNameLike/:query', 'UserController.findNameLike')
-
+  Route.get("getAllPurchases/:id", "UserController.getAllPurchases");
+  Route.get("getAllPaidPurchases/:id", "UserController.getAllPaidPurchases");
+  Route.get(
+    "getAllUnPaidPurchases/:id",
+    "UserController.getAllUnPaidPurchases"
+  );
+  Route.get("getAllAdmin", "UserController.getAllAdmin");
+  Route.get("findBy", "UserController.findBy");
+  Route.get("findNameLike/:query", "UserController.findNameLike");
 })
-  .middleware([
-    'auth', 'typeUser:master,query,edit,self'
-  ])
-  .prefix('user')
+  .middleware(["auth", "typeUser:master,query,edit,self"])
+  .prefix("user");
