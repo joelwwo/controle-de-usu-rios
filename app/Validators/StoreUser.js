0dd0ad5f2738744435c664e1ed8d4ec20a0f683f
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2021-05-07 00:59:15 -0300

    Correção de validação

--- app/Validators/StoreUser.js
+++ app/Validators/StoreUser.js
@@ -1,44 +1,44 @@
-'use strict'
+"use strict";
 
 class StoreUser {
+  get rules() {
+    return {
+      name: "required|min:2",
+      email: "email|unique:users",
+      password: "min:6",
+      cpf: "min:11|max:11|unique:users",
+      type: "in:master,edit,query,user",
+    };
+  }
 
-    get rules() {
-        return {
-            name: 'required|min:2',
-            email: 'email|unique:users',
-            password: 'min:6',
-            cpf: 'min:11|max:11',
-            type: 'in:master,edit,query,user'
-        }
-    }
+  get messages() {
+    return {
+      "name.required": "Informe o nome.",
+      "name.min": "Por favor, informe um nome com mais de 1 caractere.",
+      "email.email": "Informe um e-mail válido.",
+      "email.unique": "Esse e-mail já está cadastrado em outro usuário",
+      "password.min": "Por favor, informe uma senha com mais de 5 caracteres.",
+      "email.unique": "Esse CPF já está cadastrado em outro usuário",
+      "cpf.min": "O CPF deve possuir 11 caracteres.",
+      "cpf.max": "O CPF deve possuir 11 caracteres.",
+      "type.in":
+        "O tipo de acesso deve ser: master, edição, consulta ou usuário.",
+    };
+  }
 
-    get messages() {
-        return {
-            'name.required': 'Informe o nome.',
-            'name.min': 'Por favor, informe um nome com mais de 1 caractere.',
-            'email.email': 'Informe um e-mail válido.',
-            'email.unique': 'Esse e-mail já está cadastrado em outro usuário',
-            'password.min': 'Por favor, informe uma senha com mais de 5 caracteres.',
-            'cpf.min': 'O CPF deve possuir 11 caracteres.',
-            'cpf.max': 'O CPF deve possuir 11 caracteres.',
-            'type.in': 'O tipo de acesso deve ser: master, edição, consulta ou usuário.',
-        }
-    }
+  get validateAll() {
+    return true;
+  }
 
-    get validateAll() {
-        return true
-    }
-
-    get sanitizationRules() {
-        return {
-            email: 'normalize_email',
-            password: 'trim',
-            cpf: 'trim',
-            name: 'trim',
-            type: 'trim',
-        }
-    }
-
+  get sanitizationRules() {
+    return {
+      email: "normalize_email",
+      password: "trim",
+      cpf: "trim",
+      name: "trim",
+      type: "trim",
+    };
+  }
 }
 
-module.exports = StoreUser
+module.exports = StoreUser;
