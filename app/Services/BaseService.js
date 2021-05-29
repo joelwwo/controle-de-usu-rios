d0f88154b468e6338a84fd82b973e34a8a5c1a57
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2021-05-29 17:50:55 -0300

    Alteração dos membros

--- app/Services/BaseService.js
+++ app/Services/BaseService.js
@@ -1,61 +1,49 @@
 class BaseService {
-    model
-    members
+  model;
+  members;
 
-    async index() {
-        try {
-            if (this.members.length)
-                return await this.model.query().with(this.members[0]).fetch()
-            return await this.model.all()
-        } catch (e) {
+  async index() {
+    try {
+      if (this.members.length)
+        return await this.model.query().with(this.members).fetch();
+      return await this.model.all();
+    } catch (e) {}
+  }
 
-        }
-    }
+  async store(data) {
+    try {
+      const targetModel = await this.model.create(data);
+      return targetModel;
+    } catch (error) {}
+  }
 
-    async store(data) {
-        try {
-            const targetModel = await this.model.create(data)
-            return targetModel
-        } catch (error) {
+  async show(id) {
+    try {
+      const targetModel = await this.model.find(id);
+      if (!targetModel) return false;
+      if (this.members.length) await targetModel.loadMany(this.members);
+      return targetModel;
+    } catch (error) {}
+  }
 
-        }
-    }
+  async update(id, data) {
+    try {
+      const targetModel = await this.model.find(id);
+      if (!targetModel) return false;
+      targetModel.merge(data);
+      await targetModel.save();
+      return targetModel;
+    } catch (error) {}
+  }
 
-    async show(id) {
-        try {
-            const targetModel = await this.model.find(id)
-            if (!targetModel) return false
-            if (this.members.length)
-                await targetModel.loadMany(this.members)
-            return targetModel
-        } catch (error) {
-
-        }
-    }
-
-    async update(id, data) {
-        try {
-            const targetModel = await this.model.find(id)
-            if (!targetModel) return false
-            targetModel.merge(data)
-            await targetModel.save()
-            return targetModel
-        } catch (error) {
-
-        }
-    }
-
-    async destroy(id) {
-        try {
-            const targetModel = await this.model.find(id)
-            if (!targetModel) return false
-            await targetModel.delete()
-            return targetModel
-        } catch (error) {
-
-        }
-    }
-
+  async destroy(id) {
+    try {
+      const targetModel = await this.model.find(id);
+      if (!targetModel) return false;
+      await targetModel.delete();
+      return targetModel;
+    } catch (error) {}
+  }
 }
 
-module.exports = BaseService
+module.exports = BaseService;
